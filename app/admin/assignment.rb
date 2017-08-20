d59b1cbaa072710d072673ae30bf70834d065cf6
Raghu Betina <raghu@firstdraft.com>
2017-08-20 21:13:05 +0000

    Whitelist columns in Assignment for ActiveAdmin

--- app/admin/assignment.rb
+++ app/admin/assignment.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Assignment do
 
+ permit_params :chores_id, :parents_id, :kids_id, :due_date, :completed
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
