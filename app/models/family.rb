881027812f1f85698b2a3662b70b37c190a0f0ed
Raghu Betina <raghu@firstdraft.com>
2017-08-20 21:12:14 +0000

    Direct association between kids and families

--- app/models/family.rb
+++ app/models/family.rb
@@ -1,6 +1,9 @@
 class Family < ApplicationRecord
   # Direct associations
 
+  belongs_to :kids,
+             :class_name => "Kid"
+
   # Indirect associations
 
   # Validations
--- app/models/kid.rb
+++ app/models/kid.rb
@@ -1,6 +1,10 @@
 class Kid < ApplicationRecord
   # Direct associations
 
+  has_many   :families,
+             :foreign_key => "kids_id",
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
