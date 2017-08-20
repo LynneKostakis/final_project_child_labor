99edf083c973bdd8d427e8c55989808e4cc0d6fa
Raghu Betina <raghu@firstdraft.com>
2017-08-20 21:12:14 +0000

    Direct association between kids and assignments

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,6 +1,9 @@
 class Assignment < ApplicationRecord
   # Direct associations
 
+  belongs_to :kids,
+             :class_name => "Kid"
+
   # Indirect associations
 
   # Validations
--- app/models/kid.rb
+++ app/models/kid.rb
@@ -1,6 +1,10 @@
 class Kid < ApplicationRecord
   # Direct associations
 
+  has_many   :assignments,
+             :foreign_key => "kids_id",
+             :dependent => :destroy
+
   has_many   :families,
              :foreign_key => "kids_id",
              :dependent => :destroy
