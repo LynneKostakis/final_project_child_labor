5cc3c840361c8a79d563022303202ab629986135
Raghu Betina <raghu@firstdraft.com>
2017-08-20 21:12:14 +0000

    Direct association between chore_categories and chores

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -1,6 +1,9 @@
 class Chore < ApplicationRecord
   # Direct associations
 
+  belongs_to :chore_categories,
+             :class_name => "ChoreCategory"
+
   has_many   :assignments,
              :foreign_key => "chores_id",
              :dependent => :destroy
--- app/models/chore_category.rb
+++ app/models/chore_category.rb
@@ -1,6 +1,10 @@
 class ChoreCategory < ApplicationRecord
   # Direct associations
 
+  has_many   :chores,
+             :foreign_key => "chore_categories_id",
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
